21a6fdf7eff9ce3e016ef35c134d5df52e1b6894
pranavrutagi-egov <pranav.rutagi@egovernments.org>
2026-07-09 14:35:46 +0530

    Whitespace support for text type values

--- src/pages/Facility/services/diagnosticReports/components/DiagnosticReportResultsTable.tsx
+++ src/pages/Facility/services/diagnosticReports/components/DiagnosticReportResultsTable.tsx
@@ -156,7 +156,9 @@ export function DiagnosticReportResultsTable({
                 highlight ? "font-bold" : "font-normal",
               )}
             >
-              <span>{component.value.value}</span>
+              <span className="whitespace-pre-line">
+                {component.value.value}
+              </span>
               {component.value.unit && (
                 <span className="text-gray-500 ml-1">
                   {component.value.unit.code || component.value.unit.display}
@@ -207,7 +209,9 @@ export function DiagnosticReportResultsTable({
                   highlight ? "font-bold" : "font-normal",
                 )}
               >
-                <span>{observation.value.value}</span>
+                <span className="whitespace-pre-line">
+                  {observation.value.value}
+                </span>
                 {observation.value.unit && (
                   <span className="text-gray-500 ml-1">
                     {observation.value.unit.code ||
